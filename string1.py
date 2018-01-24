ce095e5856fb276acdd7b7aa48c6ffbbd9b356ca
pallvi30 <pallvi30lpu@gmail.com>
2018-01-24 14:08:51 +0530

    string

---
+++ string1.py
@@ -0,0 +1,13 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x="pallvi"
+>>> x=x+ "is good girl"
+>>> x
+'pallviis good girl'
+>>> x[1]
+'a'
+>>> x[2:3]
+'l'
+>>> x[-3]
+'i'
+>>> 
