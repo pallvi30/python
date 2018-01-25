6129d3206c0271f915f3687623e33b1537c7e899
pallvi30 <pallvi30lpu@gmail.com>
2018-01-25 12:09:53 +0530

    List

---
+++ list1.py
@@ -0,0 +1,21 @@
+CAP=["Pallvi", 23, "MCA"]
+print(CAP)
+CAP.append('Pallvi Arora')
+print(CAP)
+empty=[]
+empty.append(1)
+print(empty)
+cap[-1]
+'MCA'
+>>> cap[:3]
+['pallvi', 23, 'MCA']
+>>> cap[2]
+'MCA'
+>>> cap[:2]
+['pallvi', 23]
+cap=[["pallvi", 23 , "A"], ["MCA", 11612003, 1618, "A"]]
+>>> cap
+[['pallvi', 23, 'A'], ['MCA', 11612003, 1618, 'A']]
+
+'i' in 'pallvi'
+True
