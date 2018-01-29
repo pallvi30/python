8565605a4bee52eb268b5037ba2db8e0de848203
pallvi30 <pallvi30lpu@gmail.com>
2018-01-29 11:03:02 +0530

    if else

---
+++ ifelse.py
@@ -0,0 +1,62 @@
+ number=input("Enter your number")
+Enter your number 12
+>>> number1=input("enter your number")
+enter your number12
+>>> number1
+'12'
+>>> number= int(input("enter your number"))
+enter your number 12
+>>> numbre
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    numbre
+NameError: name 'numbre' is not defined
+>>> number
+12
+>>> string= str(input("enter your name"))
+enter your namepallvi
+>>> 
+>>> name
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    name
+NameError: name 'name' is not defined
+>>> string
+'pallvi'
+>>> type(string)
+<class 'str'>
+number=int(input("enter your number"))
+enter your number 12
+>>> print("Your number is:" , number)
+Your number is: 12
+name= str(input("enter your name"))
+enter your name Pallvi
+>>> Regno= int(input("enter your Regno."))
+enter your Regno. 11612003
+>>> coursecode= str(input("enter your coursecode"))
+enter your coursecode CAP630
+>>> print(name,Regno,coursecode)
+Pallvi 11612003  CAP630
+no1=int(input("enter your no."))
+enter your no. 3
+>>> no2=int(input("enter your no."))
+enter your no. 8
+sum=no1+no2
+>>> print(sum)
+11
+/* type of loop*/
+entry loop
+exit loop
+Odd loop:- odd loop is that in which we don't how many type loop will run.
+>>> Hot key:- hot key example in file we press Ctrl+F . F is hot key.
+\
+------------------------------------------------------------------------------------
+word="Hi I am happy"
+rword=word[::-1]
+if word==rword:
+	print("string is palindrome", Number)
+else:
+	print("both are equal")
+
+	
+both are equal
