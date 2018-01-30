a27681d3700af49c1f6e93cd8bc509036f26ee13
pallvi30 <pallvi30lpu@gmail.com>
2018-01-30 10:01:24 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,45 @@
+x=int(input("enter an integer :"))
+enter an integer :42
+>>> if x<0:
+	x=0
+	print("Negative number")
+elif x==0:
+	print('zero')
+elif x==1:
+	print('single')
+else:
+	print('more')
+
+	
+output:- more
+>>> ----------------------------------------
+for i in range(10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+---------------
+for i in range(4,10,2):
+	print(i)
+
+	
+4
+6
+8
+----------------
+for i in range(-50,-100,-10):
+	print(i)
+-50
+-60
+-70
+-80
+-90
